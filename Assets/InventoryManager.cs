a38996492acf3f5b1d8753e3dc16ea38a52295d6
Dead245 <116427900+Dead245@users.noreply.github.com>
2023-07-04 19:31:46 -0500

    Created ItemSlot prefab and slot generation

--- Assets/InventoryManager.cs
+++ Assets/InventoryManager.cs
@@ -13,7 +13,10 @@ public class InventoryManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        for (int i = 0; i < slotCount; i++)
+        {
+            Instantiate(slotPrefab,this.transform);
+        }
     }
 
     // Update is called once per frame
